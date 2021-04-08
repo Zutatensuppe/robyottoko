8a01b6621954e987c8f7743ed1b8f7ef9e69def8
Zutatensuppe <oo.para@gmail.com>
2021-04-08 23:36:37 +0200

    rename variable

--- src/mod/ModuleStorage.js
+++ src/mod/ModuleStorage.js
@@ -1,12 +1,12 @@
 class ModuleStorage {
-  constructor(db, user_id) {
+  constructor(db, userId) {
     this.db = db
     this.table = 'module'
-    this.user_id = user_id
+    this.userId = userId
   }
   load (key, def) {
     try {
-      const where = { user_id: this.user_id, key }
+      const where = { user_id: this.userId, key }
       const row = this.db.get(this.table, where)
       const data = row ? JSON.parse('' + row.data) : null
       return data ? Object.assign({}, def, data) : def
@@ -16,7 +16,7 @@ class ModuleStorage {
     }
   }
   save (key, rawData) {
-    const where = { user_id: this.user_id, key }
+    const where = { user_id: this.userId, key }
     const data = JSON.stringify(rawData)
     const dbData = Object.assign({}, where, {data})
     this.db.upsert(this.table, dbData, where)
