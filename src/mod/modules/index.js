9a18a64fd08fedb29b2a9307e5ef40a2cbf77ccd
Zutatensuppe <oo.para@gmail.com>
2021-06-20 15:24:34 +0200

    basic vote feature

--- src/mod/modules/index.js
+++ src/mod/modules/index.js
@@ -1,6 +1,7 @@
 module.exports = [
   require('./GeneralModule.js'),
   require('./SongrequestModule.js'),
+  require('./VoteModule.js'),
   require('./SpeechToTextModule.js'),
   require('./DrawcastModule.js'),
 ]
