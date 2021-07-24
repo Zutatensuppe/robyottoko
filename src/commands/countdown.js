ab56a27bd9fbd59d6e41cf68f47e6eb404712def
Zutatensuppe <oo.para@gmail.com>
2021-07-24 17:18:22 +0200

    allow usage of placeholders in countdown commands

--- src/commands/countdown.js
+++ src/commands/countdown.js
@@ -8,47 +8,50 @@ const countdown = (
   /** @type string */ target,
   context,
   /** @type string */ msg,
-) => {
-  const say = fn.sayFn(client, target)
-
-  const actions = []
-  const t = (settings.type || 'auto')
-
-  if (t === 'auto') {
-    const steps = settings.steps
-    const interval = settings.interval || (1 * fn.SECOND)
-    const msgStep = settings.step || "{step}"
-    const msgIntro = settings.intro || null
-    const msgOutro = settings.outro || null
-    if (msgIntro) {
-      actions.push(async () => say(msgIntro.replace(/\{steps\}/g, steps)))
-      actions.push(async () => await fn.sleep(interval))
+  ) => {
+    const sayFn = fn.sayFn(client, target)
+    const say = async (text) => {
+      return sayFn(await fn.parseResponseText(text, command, context))
     }
 
-    for (let step = steps; step > 0; step--) {
-      actions.push(async () => say(msgStep
-        .replace(/\{steps\}/g, steps)
-        .replace(/\{step\}/g, step)
-      ))
-      actions.push(async () => await fn.sleep(interval))
-    }
+    const actions = []
+    const t = (settings.type || 'auto')
 
-    if (msgOutro) {
-      actions.push(async () => say(msgOutro.replace(/\{steps\}/g, steps)))
-    }
-  } else if (t === 'manual') {
-    for (const a of settings.actions) {
-      if (a.type === 'text') {
-        actions.push(async () => say(a.value))
-      } else if (a.type ==='delay') {
-        actions.push(async () => await fn.sleep(parseInt(a.value, 10) || 0))
+    if (t === 'auto') {
+      const steps = settings.steps
+      const interval = settings.interval || (1 * fn.SECOND)
+      const msgStep = settings.step || "{step}"
+      const msgIntro = settings.intro || null
+      const msgOutro = settings.outro || null
+      if (msgIntro) {
+        actions.push(async () => await say(msgIntro.replace(/\{steps\}/g, steps)))
+        actions.push(async () => await fn.sleep(interval))
+      }
+
+      for (let step = steps; step > 0; step--) {
+        actions.push(async () => say(msgStep
+          .replace(/\{steps\}/g, steps)
+          .replace(/\{step\}/g, step)
+        ))
+        actions.push(async () => await fn.sleep(interval))
+      }
+
+      if (msgOutro) {
+        actions.push(async () => await say(msgOutro.replace(/\{steps\}/g, steps)))
+      }
+    } else if (t === 'manual') {
+      for (const a of settings.actions) {
+        if (a.type === 'text') {
+          actions.push(async () => say(a.value))
+        } else if (a.type === 'delay') {
+          actions.push(async () => await fn.sleep(parseInt(a.value, 10) || 0))
+        }
       }
     }
-  }
 
-  for (let i = 0; i < actions.length; i++) {
-    await actions[i]()
+    for (let i = 0; i < actions.length; i++) {
+      await actions[i]()
+    }
   }
-}
 
 module.exports = countdown
