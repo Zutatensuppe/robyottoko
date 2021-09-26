4c9a9d6f573ec3fcaccb6dc38c388b45daa30b6a
Zutatensuppe <oo.para@gmail.com>
2021-09-26 16:45:47 +0200

    meta

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -18,17 +18,61 @@ import ForgotPassword from './views/ForgotPassword.vue'
   const router = VueRouter.createRouter({
     history: VueRouter.createWebHistory(),
     routes: [
-      { name: 'index', path: '/', component: Index },
-      { name: 'login', path: '/login', component: Login },
-      { name: 'register', path: '/register', component: Register },
-      { name: 'password-reset', path: '/password-reset', component: PasswordReset },
-      { name: 'forgot-password', path: '/forgot-password', component: ForgotPassword },
-      { name: 'commands', path: '/commands/', component: Commands },
-      { name: 'variables', path: '/variables/', component: Variables },
-      { name: 'sr', path: '/sr/', component: SongRequest },
-      { name: 'speech-to-text', path: '/speech-to-text/', component: SpeechToText },
-      { name: 'drawcast', path: '/drawcast/', component: Drawcast },
-      { name: 'settings', path: '/settings/', component: Settings },
+      {
+        name: 'index', path: '/', component: Index, meta: {
+          title: 'Hyottoko.club',
+        }
+      },
+      {
+        name: 'login', path: '/login', component: Login, meta: {
+          title: 'Login',
+        }
+      },
+      {
+        name: 'register', path: '/register', component: Register, meta: {
+          title: 'Register',
+        }
+      },
+      {
+        name: 'password-reset', path: '/password-reset', component: PasswordReset, meta: {
+          title: 'Password Reset',
+        }
+      },
+      {
+        name: 'forgot-password', path: '/forgot-password', component: ForgotPassword, meta: {
+          title: 'Forgot Password',
+        }
+      },
+      {
+        name: 'commands', path: '/commands/', component: Commands, meta: {
+          title: 'Commands',
+        }
+      },
+      {
+        name: 'variables', path: '/variables/', component: Variables, meta: {
+          title: 'Variables',
+        }
+      },
+      {
+        name: 'sr', path: '/sr/', component: SongRequest, meta: {
+          title: 'Song Request',
+        }
+      },
+      {
+        name: 'speech-to-text', path: '/speech-to-text/', component: SpeechToText, meta: {
+          title: 'Speech to text',
+        }
+      },
+      {
+        name: 'drawcast', path: '/drawcast/', component: Drawcast, meta: {
+          title: 'Drawcast',
+        }
+      },
+      {
+        name: 'settings', path: '/settings/', component: Settings, meta: {
+          title: 'Settings',
+        }
+      },
     ],
   })
 
@@ -37,6 +81,8 @@ import ForgotPassword from './views/ForgotPassword.vue'
       document.documentElement.classList.remove(`view-${String(from.name)}`)
     }
     document.documentElement.classList.add(`view-${String(to.name)}`)
+
+    document.title = `${to.meta.title}`
   })
 
   const app = Vue.createApp(App)
