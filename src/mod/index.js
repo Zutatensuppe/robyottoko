16939cc27c228204803f4f39951354a8da524f89
Zutatensuppe <oo.para@gmail.com>
2021-09-30 13:14:32 +0200

    remove obsolete index files

--- src/mod/index.js
+++
@@ -1,5 +0,0 @@
-import ModuleManager from './ModuleManager.js'
-import ModuleStorage from './ModuleStorage.js'
-import modules from './modules'
-
-export default { ModuleManager, ModuleStorage, modules }
--- src/net/index.js
+++
@@ -1,6 +0,0 @@
-import WebServer from './WebServer.js'
-import WebSocketServer from './WebSocketServer.js'
-import Auth from './Auth.js'
-import TwitchClientManager from './TwitchClientManager.js'
-
-export default { WebServer, WebSocketServer, Auth, TwitchClientManager }
