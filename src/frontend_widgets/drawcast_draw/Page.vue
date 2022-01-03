8f714240e66bd9851e5b9ebe089d622ee4edcaee
Zutatensuppe <oo.para@gmail.com>
2022-01-03 23:47:59 +0100

    adjust brigthness required for white border

--- src/frontend_widgets/drawcast_draw/Page.vue
+++ src/frontend_widgets/drawcast_draw/Page.vue
@@ -290,7 +290,7 @@ const hexIsLight = (color: string) => {
   const c_g = parseInt(hex.substr(2, 2), 16);
   const c_b = parseInt(hex.substr(4, 2), 16);
   const brightness = (c_r * 299 + c_g * 587 + c_b * 114) / 1000;
-  return brightness > 155;
+  return brightness > 69;
 };
 
 export default defineComponent({
