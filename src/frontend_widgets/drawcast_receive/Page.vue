8ba45ecf3f43e0869cd32de8bdd652da50e70848
Zutatensuppe <oo.para@gmail.com>
2022-03-29 22:32:40 +0200

    reduce media queue code

--- src/frontend_widgets/drawcast_receive/Page.vue
+++ src/frontend_widgets/drawcast_receive/Page.vue
@@ -1,5 +1,9 @@
 <template>
-  <div v-if="imgstyle" :style="imgstyle"></div>
+  <media-queue-element
+    ref="q"
+    :timeBetweenMediaMs="500"
+    :displayLatestForever="displayLatestForever"
+  />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -7,147 +11,46 @@ import WsClient from "../../frontend/WsClient";
 import { DrawcastImage } from "../../mod/modules/DrawcastModuleCommon";
 import { SoundMediaFile } from "../../types";
 import util from "../util";
-
-interface Media {
-  image: {
-    url: string;
-  };
-}
-
-interface QueueItem {
-  media: Media;
-  playsound: boolean;
-}
+import MediaQueueElement from "../MediaQueueElement.vue";
 
 interface ComponentData {
   ws: WsClient | null;
-  queue: QueueItem[];
-  worker: any; // null | number (setInterval)
-  imgstyle: null | Record<string, string>;
   displayDuration: number;
   displayLatestForever: boolean;
-  displayLatestAutomatically: boolean;
   notificationSound: SoundMediaFile | null;
-  notificationSoundAudio: any;
-  latestResolved: boolean;
   images: any[];
 }
 
 export default defineComponent({
+  components: {
+    MediaQueueElement,
+  },
   data(): ComponentData {
     return {
       ws: null,
-      queue: [],
-      worker: null,
-      imgstyle: null,
       displayDuration: 5000,
       displayLatestForever: false,
-      displayLatestAutomatically: false,
-
       notificationSound: null,
-      notificationSoundAudio: null,
-      latestResolved: true,
-
       images: [],
     };
-  },
-  methods: {
-    async playone(item: QueueItem): Promise<void> {
-      return new Promise(async (resolve) => {
-        this.latestResolved = false;
-        await this.prepareImage(item.media.image.url);
-
-        this.imgstyle = {
-          backgroundImage: `url(${item.media.image.url})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          height: "100%",
-        };
-
-        if (item.playsound && this.notificationSoundAudio) {
-          this.notificationSoundAudio.play();
-        }
-
-        setTimeout(() => {
-          if (!this.displayLatestForever) {
-            this.imgstyle = null;
-          }
-          this.latestResolved = true;
-          resolve();
-        }, this.displayDuration);
-      });
-    },
-    addQueue(media: Media, playsound: boolean) {
-      this.queue.push({ media, playsound });
-      if (this.worker) {
-        return;
-      }
-
-      const next = async () => {
-        if (this.queue.length === 0) {
-          clearInterval(this.worker);
-          this.worker = null;
-          return;
-        }
-        const item = this.queue.shift();
-        if (!item) {
-          clearInterval(this.worker);
-          this.worker = null;
-          return;
-        }
-        await this.playone(item);
-        this.worker = setTimeout(next, 500); // this much time in between media
-      };
-      this.worker = setTimeout(next, 500);
-    },
-    async prepareImage(urlpath: string): Promise<void> {
-      return new Promise((resolve) => {
-        const imgLoad = new Image();
-        imgLoad.src = urlpath;
-        this.$nextTick(() => {
-          if (imgLoad.loaded) {
-            resolve();
-          } else {
-            imgLoad.onload = resolve;
-          }
-        });
-      });
-    },
-    playmedia(media: Media, playsound: boolean) {
-      this.addQueue(media, playsound);
-    },
   },
   mounted() {
     this.ws = util.wsClient("drawcast");
 
     this.ws.onMessage("init", (data) => {
       // submit button may not be empty
-      this.displayDuration = data.settings.displayDuration;
       this.displayLatestForever = data.settings.displayLatestForever;
       this.notificationSound = data.settings.notificationSound;
-      this.displayLatestAutomatically =
-        data.settings.displayLatestAutomatically;
-
-      // if previously set to 'display forever' and something is
-      // currently displaying because of that, hide it
-      if (!this.displayLatestForever && this.latestResolved) {
-        this.imgstyle = null;
-      }
-      if (this.notificationSound) {
-        this.notificationSoundAudio = new Audio(this.notificationSound.urlpath);
-        this.notificationSoundAudio.volume =
-          this.notificationSound.volume / 100.0;
-      }
       this.images = data.images.map((image: DrawcastImage) => image.path);
 
-      if (this.displayLatestAutomatically && this.images.length > 0) {
-        this.playmedia(
-          {
-            image: { url: this.images[0] },
-          },
-          false
-        );
+      if (data.settings.displayLatestAutomatically && this.images.length > 0) {
+        this.$refs["q"].playmedia({
+          sound: { file: "", filename: "", urlpath: "", volume: 100 },
+          image: { file: "", filename: "", urlpath: "" },
+          twitch_clip: { url: "", volume: 100 },
+          image_url: this.images[0],
+          minDurationMs: this.displayDuration,
+        });
       }
     });
     this.ws.onMessage(
@@ -155,12 +58,17 @@ export default defineComponent({
       (data: { nonce: string; img: string; mayNotify: boolean }) => {
         this.images.unshift(data.img);
         this.images = this.images.slice(0, 20);
-        this.playmedia(
-          {
-            image: { url: data.img },
-          },
-          data.mayNotify
-        );
+
+        this.$refs["q"].playmedia({
+          sound:
+            data.mayNotify && this.notificationSound
+              ? this.notificationSound
+              : { file: "", filename: "", urlpath: "", volume: 100 },
+          image: { file: "", filename: "", urlpath: "" },
+          twitch_clip: { url: "", volume: 100 },
+          image_url: data.img,
+          minDurationMs: this.displayDuration,
+        });
       }
     );
     this.ws.connect();
