cdd68ada3dd242f0225bb8af7213cc2a7676a03b
Zutatensuppe <oo.para@gmail.com>
2022-07-24 13:05:57 +0200

    add test for findIdxFuzzy

--- src/fn.test.ts
+++ src/fn.test.ts
@@ -6,6 +6,7 @@ import {
   doReplacements,
   findIdxBySearchInOrder,
   findIdxBySearch,
+  findIdxFuzzy,
   findIdxBySearchExactPart,
   parseCommandFromTriggerAndMessage,
 } from './fn'
@@ -31,90 +32,186 @@ test.each([
   expect(actual).toBe(expected)
 })
 
-test('joinIntoChunks', () => {
-  let actual = joinIntoChunks(['hyottoko', 'van', 'megaport'], ', ', 12)
-  let expected = ['hyottoko', 'van', 'megaport']
-  expect(actual).toStrictEqual(expected)
+describe('fn.joinIntoChunks', () => {
+  test.each([
+    {
+      strings: ['hyottoko', 'van', 'megaport'],
+      glue: ', ',
+      maxChunkLen: 12,
+      expected: ['hyottoko', 'van', 'megaport'],
+    },
+    {
+      strings: ['hyottoko', 'van', 'port'],
+      glue: ', ',
+      maxChunkLen: 12,
+      expected: ['hyottoko', 'van, port'],
+    },
+    {
+      strings: ['hyottoko', 'van', 'megaport'],
+      glue: ', ',
+      maxChunkLen: 13,
+      expected: ['hyottoko, van', 'megaport'],
+    },
+  ])('joinIntoChunks', ({ strings, glue, maxChunkLen, expected }) => {
+    const actual = joinIntoChunks(strings, glue, maxChunkLen)
+    expect(actual).toStrictEqual(expected)
+  })
+})
 
-  actual = joinIntoChunks(['hyottoko', 'van', 'port'], ', ', 12)
-  expected = ['hyottoko', 'van, port']
-  expect(actual).toStrictEqual(expected)
+describe('fn.parseISO8601Duration', () => {
+  // @see https://en.wikipedia.org/wiki/ISO_8601#Durations
+  test.each`
+  duration            | expected
+  ${'P1Y'}            | ${31536000000}
+  ${'P1M'}            | ${2592000000}
+  ${'P1D'}            | ${24 * 3600000}
+  ${'PT1H'}           | ${3600000}
+  ${'PT1M'}           | ${60000}
+  ${'PT1S'}           | ${1000}
+  ${'P1Y1M1D'}        | ${34214400000}
+  ${'P1Y3M3D'}        | ${39571200000}
+  ${'P1Y3M3DT7H6M'}   | ${39596760000}
+  ${'P1Y3M3DT7H6M9S'} | ${39596769000}
+  ${'PT3M14S'}        | ${194000}
+  ${'PT3M15S'}        | ${195000}
+  ${'PT3M27S'}        | ${207000}
+  ${'PT3M44S'}        | ${224000}
+  ${'PT4M3S'}         | ${243000}
+  `('parseISO8601Duration $duration', ({ duration, expected }) => {
+    // P(n)Y(n)M(n)DT(n)H(n)M(n)S
+    const actual = parseISO8601Duration(duration)
+    expect(actual).toStrictEqual(expected)
+  })
+})
 
-  actual = joinIntoChunks(['hyottoko', 'van', 'megaport'], ', ', 13)
-  expected = ['hyottoko, van', 'megaport']
+test.each([
+  {
+    array: [
+      'Welle Erdball - Starfighter F104S',
+      'Smash Mouth - All Star (Official Music Video)',
+    ],
+    search: 'all star',
+    expected: 1,
+  },
+])('findIdxFuzzy', ({ array, search, expected }) => {
+  const actual = findIdxFuzzy(array, search)
   expect(actual).toStrictEqual(expected)
 })
 
-// @see https://en.wikipedia.org/wiki/ISO_8601#Durations
-test.each`
-duration            | expected
-${'P1Y'}            | ${31536000000}
-${'P1M'}            | ${2592000000}
-${'P1D'}            | ${24 * 3600000}
-${'PT1H'}           | ${3600000}
-${'PT1M'}           | ${60000}
-${'PT1S'}           | ${1000}
-${'P1Y1M1D'}        | ${34214400000}
-${'P1Y3M3D'}        | ${39571200000}
-${'P1Y3M3DT7H6M'}   | ${39596760000}
-${'P1Y3M3DT7H6M9S'} | ${39596769000}
-${'PT3M14S'}        | ${194000}
-${'PT3M15S'}        | ${195000}
-${'PT3M27S'}        | ${207000}
-${'PT3M44S'}        | ${224000}
-${'PT4M3S'}         | ${243000}
-`('parseISO8601Duration $duration', ({ duration, expected }) => {
-  // P(n)Y(n)M(n)DT(n)H(n)M(n)S
-  const actual = parseISO8601Duration(duration)
-  expect(actual).toStrictEqual(expected)
-})
-
-test.each`
-array             | search   | expected
-${['abc', 'lel']} | ${'lel'} | ${1}
-${['Trio Da Da Da Official Video', 'Panda! Go Panda! (Panda Kopanda) intro theme']} | ${'da da da'} | ${0}
-${['Panda! Go Panda! (Panda Kopanda) intro theme', 'Trio Da Da Da Official Video']} | ${'da da da'} | ${1}
-`('findIdxBySearchExactPart', ({ array, search, expected }) => {
+test.each([
+  {
+    array: ['abc', 'lel'],
+    search: 'lel',
+    expected: 1,
+  },
+  {
+    array: ['Trio Da Da Da Official Video', 'Panda! Go Panda! (Panda Kopanda) intro theme'],
+    search: 'da da da',
+    expected: 0,
+  },
+  {
+    array: ['Panda! Go Panda! (Panda Kopanda) intro theme', 'Trio Da Da Da Official Video'],
+    search: 'da da da',
+    expected: 1,
+  },
+])('findIdxBySearchExactPart', ({ array, search, expected }) => {
   const actual = findIdxBySearchExactPart(array, search)
   expect(actual).toStrictEqual(expected)
 })
 
-test.each`
-array             | search   | expected
-${['abc', 'lel']} | ${'lel'} | ${1}
-${['Trio Da Da Da Official Video', 'Panda! Go Panda! (Panda Kopanda) intro theme']} | ${'da da da'} | ${0}
-${['Panda! Go Panda! (Panda Kopanda) intro theme', 'Trio Da Da Da Official Video']} | ${'da da da'} | ${1}
-${['Błogosławieni Miłosierni (Krysiek Remix)', 'Blogoslawieni Milosierni (Krysiek Remix)']} | ${'blogo'} | ${0}
-`('findIdxBySearchInOrder', ({ array, search, expected }) => {
+test.each([
+  {
+    array: ['abc', 'lel'],
+    search: 'lel',
+    expected: 1,
+  },
+  {
+    array: ['Trio Da Da Da Official Video', 'Panda! Go Panda! (Panda Kopanda) intro theme'],
+    search: 'da da da',
+    expected: 0,
+  },
+  {
+    array: ['Panda! Go Panda! (Panda Kopanda) intro theme', 'Trio Da Da Da Official Video'],
+    search: 'da da da',
+    expected: 1,
+  },
+  {
+    array: ['Błogosławieni Miłosierni (Krysiek Remix)', 'Blogoslawieni Milosierni (Krysiek Remix)'],
+    search: 'blogo',
+    expected: 0,
+  },
+])('findIdxBySearchInOrder', ({ array, search, expected }) => {
   const actual = findIdxBySearchInOrder(array, search)
   expect(actual).toStrictEqual(expected)
 })
 
-test.each`
-array             | search   | expected
-${['abc', 'lel']} | ${'lel'} | ${1}
-`('findIdxBySearch', ({ array, search, expected }) => {
+test.each([
+  {
+    array: ['abc', 'lel'],
+    search: 'lel',
+    expected: 1,
+  },
+])('findIdxBySearch', ({ array, search, expected }) => {
   const actual = findIdxBySearch(array, search)
   expect(actual).toStrictEqual(expected)
 })
 
-test.each`
-msg                | command       | commandExact | expected
-${'!sr good good'} | ${'!sr good'} | ${false}     | ${{ name: '!sr good', args: ['good'] }}
-${'!sr good'}      | ${'!sr good'} | ${false}     | ${{ name: '!sr good', args: [] }}
-${'!sr hello'}     | ${'!sr'}      | ${false}     | ${{ name: '!sr', args: ['hello'] }}
-${'!sr'}           | ${'!sr'}      | ${false}     | ${{ name: '!sr', args: [] }}
-${'!sr good good'} | ${'!sr good'} | ${true}      | ${null}
-${'!sr good'}      | ${'!sr good'} | ${true}      | ${{ name: '!sr good', args: [] }}
-${'!sr hello'}     | ${'!sr'}      | ${true}      | ${null}
-${'!sr'}           | ${'!sr'}      | ${true}      | ${{ name: '!sr', args: [] }}
-`('parseKnownCommandFromTriggerAndMessage $msg', ({ msg, command, commandExact, expected }) => {
-  const trigger = { type: 'command', data: { command, commandExact } } as CommandTrigger
-  const actual = parseCommandFromTriggerAndMessage(msg, trigger)
-  expect(actual).toStrictEqual(expected)
+describe('fn.parseKnownCommandFromTriggerAndMessage', () => {
+  test.each([
+    {
+      msg: '!sr good good',
+      command: '!sr good',
+      commandExact: false,
+      expected: { name: '!sr good', args: ['good'] },
+    },
+    {
+      msg: '!sr good',
+      command: '!sr good',
+      commandExact: false,
+      expected: { name: '!sr good', args: [] },
+    },
+    {
+      msg: '!sr hello',
+      command: '!sr',
+      commandExact: false,
+      expected: { name: '!sr', args: ['hello'] },
+    },
+    {
+      msg: '!sr',
+      command: '!sr',
+      commandExact: false,
+      expected: { name: '!sr', args: [] },
+    },
+    {
+      msg: '!sr good good',
+      command: '!sr good',
+      commandExact: true,
+      expected: null,
+    },
+    {
+      msg: '!sr good',
+      command: '!sr good',
+      commandExact: true,
+      expected: { name: '!sr good', args: [] },
+    },
+    {
+      msg: '!sr hello',
+      command: '!sr',
+      commandExact: true,
+      expected: null,
+    },
+    {
+      msg: '!sr',
+      command: '!sr',
+      commandExact: true,
+      expected: { name: '!sr', args: [] },
+    },
+  ])('parseKnownCommandFromTriggerAndMessage $msg', ({ msg, command, commandExact, expected }) => {
+    const trigger = { type: 'command', data: { command, commandExact } } as CommandTrigger
+    const actual = parseCommandFromTriggerAndMessage(msg, trigger)
+    expect(actual).toStrictEqual(expected)
+  })
 })
-
-
 
 describe('fn.doReplacements', () => {
   test.each([
