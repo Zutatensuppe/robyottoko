df85465949563f7ceb7723f39e032b42e2132993
Zutatensuppe <oo.para@gmail.com>
2022-11-18 03:22:27 +0100

    build

--- build/server/index.js
+++ build/server/index.js
@@ -7272,9 +7272,9 @@ class PomoModule {
 
 var buildEnv = {
     // @ts-ignore
-    buildDate: "2022-11-18T01:22:51.777Z",
+    buildDate: "2022-11-18T02:17:52.682Z",
     // @ts-ignore
-    buildVersion: "1.36.0",
+    buildVersion: "1.36.1",
 };
 
 const log$3 = logger('StreamStatusUpdater.ts');
