fb4019c79af474be3184c0ff792ede882a037eec
Zutatensuppe <oo.para@gmail.com>
2023-10-08 21:13:50 +0200

    don't send a roulette message when there are no entries

--- src/effect/RouletteEffect.ts
+++ src/effect/RouletteEffect.ts
@@ -3,6 +3,9 @@ import { Effect } from './Effect'
 
 export class RouletteEffect extends Effect<RouletteEffectData> {
   async apply(): Promise<void> {
+    if (this.effect.data.entries.length === 0) {
+      return
+    }
     this.notifyWs('general', {
       event: 'roulette',
       data: this.effect.data,
